b02c16c05223b5f195756de9b6ba9a5d553cad81
Mohammed Isa <mohd.itcs@gmail.com>
2017-10-15 14:41:01 +0300

    init

---
+++ src/Filter.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Arados\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+abstract class Filter
+{
+    /**
+     * Eloquent builder.
+     *
+     * @var Builder
+     */
+    protected $builder;
+
+    /**
+     * Http request.
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * Filter constructor.
+     *
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Apply the filters on the query.
+     *
+     * @param $builder
+     * @return Builder
+     */
+    public function apply($builder)
+    {
+        $this->builder = $builder;
+
+        foreach ($this->getFilters() as $filter) {
+            call_user_func([$this, $filter]);
+        }
+
+        return $this->builder;
+    }
+
+    /**
+     * Fetch all relevant filters from the request.
+     *
+     * @throws FiltersNotDefinedException
+     * @return array
+     */
+    protected function getFilters()
+    {
+        $filters = array_diff(get_class_methods($this), ['__construct', 'apply', 'getFilters']);
+
+        return array_intersect($filters, array_keys($this->request->query()));
+    }
+}
